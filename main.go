bdf298615a20416393b5e05dec481c7e1e0406ac
Korbinian Karl <korbi@fs.cs.hm.edu>
2018-04-23 20:27:10 +0200

    added hello world

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import ("fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	http.HandleFunc("/", hello)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "Hello World")
 }
