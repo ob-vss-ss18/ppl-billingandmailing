a7012ba56d8faf29c8ae136cd2c17a0ee794f66d
Korbinian Karl <korbi@fs.cs.hm.edu>
2018-04-24 23:05:36 +0200

    Added travis files

--- main.go
+++ main.go
@@ -10,8 +10,6 @@ func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server started...")
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))		//  assigned by Heroku as the PORT environment variable
-
-
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
