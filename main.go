6864cfc6206654f6daf888de7c73aa876c691a2e
Elias Porcio <elias.porcio@hm.edu>
2018-05-10 12:09:57 +0200

    Added Postgresql test code

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import ("fmt"
 	"net/http"
 	"log"
 	"os"
+	"database/sql"
+	"github.com/lib/pq"
 )
 
 func main() {
@@ -14,4 +16,23 @@ func main() {
 
 func handler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "Hello World")
+}
+
+func testDB(){
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Println(err);
+	}
+
+	db.Exec(`CREATE TABLE TEST_TABLE(
+		ID INT NOT NULL,
+		NAME TEST_FIELD
+		PRIMARY KEY (ID)
+	);`)
+
+	db.Exec(`INSERT INTO TEST_TABLE
+		VALUES(2, 4, 8)
+	);`)
+
+	db.Exec(`SELECT * FROM TEST_TABLE`)
 }
