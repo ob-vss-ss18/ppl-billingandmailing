250d29c06179e6290ba640ee6fa26209ba841be8
Elias Porcio <elias.porcio@hm.edu>
2018-06-13 13:46:33 +0200

    PDF creation with example function. Closes #8

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import ("fmt"
 	"net/http"
 	"log"
 	"os"
+	"ppl-billingandmailing/pdf"
 )
 
 func main() {
+	pdf.InvoiceExample()
+
 	http.HandleFunc("/", handler)
 	fmt.Println("Server started...")
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))		//  assigned by Heroku as the PORT environment variable
